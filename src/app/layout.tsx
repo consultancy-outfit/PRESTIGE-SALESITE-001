f03050130a91c15a9e5dbd74009e4ac4d68aab24
talhawajdan <134398365+talhawajdandanny@users.noreply.github.com>
2025-08-08 09:28:29 +0500

    #update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "https://prestigefin.netlify.app/og-logo.png",
+        url: "https://prestigefin.netlify.app/favicon.png",
         type: "image/png",
         width: 1200,
         height: 630,
