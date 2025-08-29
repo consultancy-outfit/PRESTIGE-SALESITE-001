2d4f2c6547c63dccf7ca9fe385719b15c97ef42d
mazamaltaf <muhammadazam.altaf@consultancyoutfit.co.uk>
2025-08-29 15:09:03 +0500

    Update page.tsx

--- src/app/(sale-site)/page.tsx
+++ src/app/(sale-site)/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <SecurityAndRegulatoryReadinessHome />
         <WhyChoosePrestigeHome/>
         <OurTechStackHome/>
-        <CustomerStoriesHome/>
+{/*         <CustomerStoriesHome/> */}
         <ReadyToGetStarted/>
       </Stack>
     </>
